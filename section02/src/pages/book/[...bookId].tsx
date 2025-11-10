d65fb22d69bb5733c3ebb6a3bb62c61a357b238e
gudrmsglgl <gudrmsglgl@gmail.com>
2025-11-10 10:53:19 +0900

    feat: prefetch 기능 사용

--- section02/src/pages/book/[...bookId].tsx
+++ section02/src/pages/book/[...bookId].tsx
@@ -2,7 +2,6 @@
 import { useRouter } from "next/router"
 export default function Page() {
     const router = useRouter();
-    console.log(router.query);
 
     return <h1>{`Book2 ${router.query.bookId}`}</h1>
 }
--- section02/src/pages/book/[bookId].tsx
+++ section02/src/pages/book/[bookId].tsx
@@ -3,6 +3,5 @@ import { useRouter } from "next/router"
 export default function Page() {
     const router = useRouter();
     const { bookId } = router.query;
-    console.log(router.query);
     return <h1>Book {bookId}</h1>
 }
