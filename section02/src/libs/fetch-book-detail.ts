3ec40dfa50437abfdbb0c0d695c04cad1e26acc7
gudrmsglgl <gudrmsglgl@gmail.com>
2025-11-20 03:13:33 +0900

    refactor: 실서버로 연동 및 배포

--- section02/src/libs/fetch-book-detail.ts
+++ section02/src/libs/fetch-book-detail.ts
@@ -2,7 +2,7 @@ import { BookData } from "@/types";
 
 export default async function fetchBookDetail(id: number): Promise<BookData | null> {
     try {
-        const url = `http://localhost:12345/book/${id}`;
+        const url = `http://onebite-books-server-main-blue-pi.vercel.app/book/${id}`;
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error();
