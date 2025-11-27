c51f5857a3352f695d62dadb9b6c7f9a348a6574
gudrmsglgl <gudrmsglgl@gmail.com>
2025-11-27 16:33:29 +0900

    feat: Navigation 추가

--- section03/src/app/(with-searchbar)/searchbar.tsx
+++ section03/src/app/(with-searchbar)/searchbar.tsx
@@ -1,19 +1,24 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Searchbar() {
-
+    const router = useRouter();
     const [search, setSearch] = useState("");
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
     }
 
+    const onSubmit = () => {
+        router.push(`/search?q=${search}`);
+    }
+
     return (
         <div>
             <input type="text" placeholder="책 검색" onChange={onChange} value={search} />
-            <button>검색</button>
+            <button onClick={onSubmit}>검색</button>
         </div>
     )
 }
