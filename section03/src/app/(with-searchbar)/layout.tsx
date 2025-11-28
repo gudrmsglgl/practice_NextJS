1a7d4c923d7d7e2460597349b538772f50aa32a8
gudrmsglgl <gudrmsglgl@gmail.com>
2025-11-28 15:57:50 +0900

    feat: UI 구성

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,14 +1,15 @@
 import { ReactNode } from "react";
-import Searchbar from "./searchbar";
+import Searchbar from "../../components/searchbar";
 
-export default function Layout(
-    { children }: { children: ReactNode }
-) {
-    return (
-        <div>
-            <h1>Searchable Layout</h1>
-            <Searchbar />
-            {children}
-        </div>
-    )
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div>
+      <Searchbar />
+      {children}
+    </div>
+  );
 }
--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,18 @@
-export default async function Page(
-    { searchParams }: { searchParams: Promise<{ q: string }> }
-) {
-    const { q } = await searchParams;
-    return <div>{`Search: ${q || 'No query'}`}</div>;
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
